243675e0d8a7602a076df0b04371ec8475b07bdf
thigalesneha <thigalesneha@gmail.com>
2016-07-19 14:55:52 +0530

    removed unnecessary comments

--- onlineBilling/src/main/java/com/javatest/onlineBilling/App.java
+++ onlineBilling/src/main/java/com/javatest/onlineBilling/App.java
@@ -10,10 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 @EnableSwagger2
 public class App 
